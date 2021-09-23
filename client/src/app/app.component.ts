fe6a16fb015a257e27a2a0da989bec310b85fe14
tajamul-hussain <tajamul.hussain@pec.edu>
2021-09-23 14:08:02 +0530

    addded angular frontend skelton and enabled cors in startup.cs

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  users:any
+
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    this.getUsers();
+
+  }
+  title = 'client';
+  getUsers(){
+    this.http.get('https://localhost:5001/api/user').subscribe((res)=>{
+      this.users=res;
+      console.log(`response came ${res}`);
+    },(err)=>{
+      console.error(err)
+    })
+
+  }
+}
