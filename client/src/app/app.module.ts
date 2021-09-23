f2a5fdfb4f9be84b8380672982273fa4c2e73722
tajamul-hussain <tajamul.hussain@pec.edu>
2021-09-23 15:26:55 +0530

    finished section2

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import{HttpClientModule} from "@angular/common/http"
+import{HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import{HttpClientModule} from "@angular/common/http"
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
